f192c4d1a4cceaed778cb0dbd560a71837aa1dc8
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-21 19:25:50 +0200

    Update query.sql

--- Which customer has placed the most orders?/query.sql
+++ Which customer has placed the most orders?/query.sql
@@ -1,1 +1,10 @@
+--Which customer has placed the most orders?
 
+SELECT 
+    orders.customer_id,
+    customers.customer_name,
+    COUNT(order_details.order_id) AS order_count
+FROM orders
+JOIN customers ON orders.customer_id = customers.customer_id
+JOIN order_details ON orders.order_id = order_details.order_id
+GROUP BY orders.customer_id;
