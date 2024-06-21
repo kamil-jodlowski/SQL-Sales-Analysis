cf7a8ea8e9c58249af5133140a774ee9614213ff
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-21 15:17:10 +0200

    Update query.sql

--- What are the details of product ID 3 in order ID 1?/query.sql
+++ What are the details of product ID 3 in order ID 1?/query.sql
@@ -1,1 +1,6 @@
+--What are the details of product ID 3 in order ID 1?
 
+SELECT products.product_name, products.category, products.price, order_details.order_id
+FROM products
+JOIN order_details ON (products.product_id = order_details.product_id)
+WHERE products.product_id = 3 AND order_details.order_id = 1 ;
