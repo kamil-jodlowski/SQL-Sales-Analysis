a66df7a8023711a02c18bb7843e44efd16fdf129
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-21 13:30:08 +0200

    Update query.sql

--- Which order has the highest total value?/query.sql
+++ Which order has the highest total value?/query.sql
@@ -1,1 +1,7 @@
+--Which order has the highest total value?
 
+SELECT order_id, price
+FROM order_details
+WHERE price = (SELECT MAX(price) FROM order_details);
+
+--Query shows which of the orders had the highest value
