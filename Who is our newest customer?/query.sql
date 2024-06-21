a3ddc1618b4a08be225413137e8d4d924884d6dc
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-21 13:45:51 +0200

    Update query.sql

--- Who is our newest customer?/query.sql
+++ Who is our newest customer?/query.sql
@@ -1,1 +1,17 @@
+--Who is our newest customer?
+
+-- If we want to find the newest customer by who placed the last order, we can use the code:
+SELECT orders.customer_id, customers.customer_name, customers.email, customers.phone
+FROM orders
+JOIN customers ON (orders.customer_id = customers.customer_id)
+WHERE orders.order_date = (SELECT MAX(orders.order_date)
+                           FROM orders);
+
+/*Please remember that this is not the latest customer registered in the record, but the latest order. 
+To find the newest customer, we should look for the one who was added to the data table last: */ 
+
+SELECT customer_id, customer_name, email, phone
+FROM customers
+ORDER BY customer_id DESC
+LIMIT 1;
 
