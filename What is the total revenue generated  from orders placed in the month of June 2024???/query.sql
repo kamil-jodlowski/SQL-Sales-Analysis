9f69b64130b03a028b9b25ee1046c64a762a2db4
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-22 16:11:21 +0200

    Update query.sql

--- What is the total revenue generated  from orders placed in the month of June 2024???/query.sql
+++ What is the total revenue generated  from orders placed in the month of June 2024???/query.sql
@@ -1,1 +1,7 @@
+--What is the total revenue generated from orders placed in the month of June 2024???
+  
+SELECT SUM(price) 
+FROM order_details
+JOIN orders ON (order_details.order_id = orders.order_id)
+WHERE orders.order_date BETWEEN '2024-06-01' AND '2024-06-30';
 
