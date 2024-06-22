15d2437e74c9a492162c577d3469cdaba20c3f52
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-22 15:48:46 +0200

    Update query.sql

--- Which customer has the highest total expenditure across all orders?/query.sql
+++ Which customer has the highest total expenditure across all orders?/query.sql
@@ -1,1 +1,8 @@
+--Which customer has the highest total expenditure across all orders?
 
+SELECT orders.customer_id, customers.customer_name , order_details.price
+FROM orders
+JOIN customers ON (orders.customer_id = customers.customer_id)
+JOIN order_details ON (order_details.order_id = orders.order_id)
+ORDER BY order_details.price DESC
+LIMIT 1;
